e27e94a916d4b69f1230bc800b4a11df4154abef
Okoye Peter <Okoyep98@gmail.com>
2019-07-15 21:44:58 +0100

    i add all the changes made

--- js/profile_fetch.js
+++ js/profile_fetch.js
@@ -1,16 +1,20 @@
 window.onload = function(){
-    if (window.XMLHttpRequest) {
-        var Xhttp = new XMLHttpRequest();
-    } else {
-        var Xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    Xhttp.onload = function(){
-        if(Xhttp.readyState == 4 && Xhttp.status == 200){
-            var article = document.getElementById("article_links");
-            article.innerHTML = Xhttp.responseText;
+    function fetch(){
+        if (window.XMLHttpRequest) {
+            var Xhttp = new XMLHttpRequest();
+        } else {
+            var Xhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        Xhttp.onload = function(){
+            if(Xhttp.readyState == 4 && Xhttp.status == 200){
+                var article = document.getElementById("article_links");
+                article.innerHTML = Xhttp.responseText;
+            }
+        }
+    
+        Xhttp.open("GET", "includes/classes/profile_fetch.php", true);
+        Xhttp.send();
     }
 
-    Xhttp.open("GET", "includes/classes/profile_fetch.php", true);
-    Xhttp.send();
+    setInterval(fetch, 1000);
 }
